4f5f1692738ba0223bbbe873e2eefdeaa69e5b09
tranquanghuy341 <tranquanghuy341@gmail.com>
2018-03-15 15:27:02 +0700

    add app weather

---
+++ app/src/main/java/com/tranquanghuy/petprojects/GuessTheCelebrity.java
@@ -0,0 +1,13 @@
+package com.tranquanghuy.petprojects;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuessTheCelebrity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_the_celebrity);
+    }
+}
